a6c6aa39da1f324a40e418406e7896cd5a59e23d
elvinhendrawan <elvinhndrwn@gmail.com>
2024-12-26 12:22:16 +0700

    initial projek

---
+++ src/main/java/com/playground/demo_vps/DemoVpsApplication.java
@@ -0,0 +1,13 @@
+package com.playground.demo_vps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoVpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playground/demo_vps/DemoVpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.demo_vps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
